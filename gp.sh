db00a55a8926921b1b39b71546184da27a20867d
Guillaume Lelarge <guillaume@lelarge.info>
2022-12-30 07:49:06 +0100

    Add progress messages to gp.sh

--- gp.sh
+++ gp.sh
@@ -8,10 +8,12 @@ test -d "$GPDIR" || mkdir -p "$GPDIR"
 test -d "$PNGDIR" || mkdir -p "$PNGDIR"
 
 # ---------- Commit Memory
+echo "Building commit memory graph"
 cp $PCDIR/commit_memory.csv $GPDIR
 gnuplot commit_memory.gp
 
 # ---------- Database size
+echo "Building database size graph"
 awk -F";" '{ print $3 }' $PCDIR/pg_database_size.csv | sort | uniq | while read db
 do
   grep $db $PCDIR/pg_database_size.csv > $GPDIR/pg_database_${db}_size.csv
@@ -19,14 +21,17 @@ do
 done
 
 # ---------- Archiver
+echo "Building archiver graph"
 cp $PCDIR/pg_stat_archiver.csv $GPDIR
 gnuplot pg_stat_archiver.gp
 
-# ---------- Archiver
+# ---------- Writer processes
+echo "Building writer processes graph"
 cp $PCDIR/pg_stat_bgwriter.csv $GPDIR
 gnuplot pg_stat_bgwriter.gp
 
 # ---------- Connections
+echo "Building connections graph"
 awk -F";" '{ print $6 }' $PCDIR/pg_stat_connections.csv | sort | uniq | while read db
 do
   grep $db $PCDIR/pg_stat_connections.csv > $GPDIR/pg_stat_connections_${db}.csv
